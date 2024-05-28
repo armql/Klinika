86d69a5b6417d4f73ca8873d8d1cb14152c6470c
am <arlindmaliqi28@gmail.com>
2024-05-28 17:06:09 +0200

    interceptors for response, request
    - JWT token added in headers, refresh token is in action

--- Klinika/klinika.client/src/api/axios.ts
+++ Klinika/klinika.client/src/api/axios.ts
@@ -9,26 +9,45 @@ import axios from "axios";
 
 // Creates an instance of axios
 const axios_instance = axios.create({
-  baseURL: "/api/",
-  timeout: 40000,
+    baseURL: "/api/",
+    timeout: 40000,
 });
 
 axios_instance.interceptors.request.use(
-  (config) => {
-    return config;
-  },
-  (error) => {
-    return Promise.reject(error);
-  }
+    (config) => {
+        const token = localStorage.getItem('jwtToken');
+        if (token) {
+            config.headers['Authorization'] = 'Bearer ' + token;
+        }
+        return config;
+    },
+    (error) => {
+        return Promise.reject(error);
+    }
 );
 
 axios_instance.interceptors.response.use(
-  (response) => {
-    return response;
-  },
-  (error) => {
-    return Promise.reject(error);
-  }
+    (response) => {
+        return response;
+    },
+    async (error) => {
+        if (error.response.status === 401 && error.config && !error.config.__isRetryRequest) {
+            error.config.__isRetryRequest = true;
+            const refreshToken = localStorage.getItem('refreshToken');
+            return axios_instance
+                .post('Auth/refreshToken', {
+                    jwtToken: localStorage.getItem('jwtToken'),
+                    refreshToken: refreshToken
+                })
+                .then((response) => {
+                    localStorage.setItem('jwtToken', response.data.jwtToken);
+                    localStorage.setItem('refreshToken', response.data.refreshToken);
+                    axios_instance.defaults.headers.common['Authorization'] = 'Bearer ' + response.data.jwtToken;
+                    return axios_instance(error.config);
+                });
+        }
+        return Promise.reject(error);
+    }
 );
 
 export default axios_instance;
